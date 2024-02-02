e7520821edd19a4cc0354353b0879daf3559b3c1
Mohamed_Hussien91 <mohamed.saad-hasanain@valeo.com>
2024-02-02 13:07:59 +0200

    fix tree visualization

--- decision_trees/utils.py
+++ decision_trees/utils.py
@@ -2,26 +2,29 @@ import numpy as np
 from matplotlib import pyplot
 
 # Some functions to plot our points and draw the models
+
+
 def plot_points(features, labels, size_of_points=100):
     X = np.array(features)
     y = np.array(labels)
-    spam = X[np.argwhere(y==1)]
-    ham = X[np.argwhere(y==0)]
+    spam = X[np.argwhere(y == 1)]
+    ham = X[np.argwhere(y == 0)]
     pyplot.scatter([s[0][0] for s in spam],
-                [s[0][1] for s in spam],
-                s = size_of_points,
-                color = 'cyan',
-                edgecolor = 'k',
-                marker = '^')
+                   [s[0][1] for s in spam],
+                   s=size_of_points,
+                   color='cyan',
+                   edgecolor='k',
+                   marker='^')
     pyplot.scatter([s[0][0] for s in ham],
-                [s[0][1] for s in ham],
-                s = size_of_points,
-                color = 'red',
-                edgecolor = 'k',
-                marker = 's')
-    #pyplot.xlabel('GRE')
-    #pyplot.ylabel('TOEFL')
-    #pyplot.legend(['Admitted','Not admitted'])
+                   [s[0][1] for s in ham],
+                   s=size_of_points,
+                   color='red',
+                   edgecolor='k',
+                   marker='s')
+    # pyplot.xlabel('GRE')
+    # pyplot.ylabel('TOEFL')
+    # pyplot.legend(['Admitted','Not admitted'])
+
 
 def plot_model(X, y, model, size_of_points=100):
     X = np.array(X)
@@ -34,27 +37,30 @@ def plot_model(X, y, model, size_of_points=100):
     Z = model.predict(np.c_[xx.ravel(), yy.ravel()])
     Z = Z.reshape(xx.shape)
     print(Z)
-    pyplot.contourf(xx, yy, Z, colors=['red', 'blue'], alpha=0.2, levels=range(-1,2))
-    pyplot.contour(xx, yy, Z,colors = 'k',linewidths = 1)
+    pyplot.contourf(xx, yy, Z, colors=[
+                    'red', 'blue'], alpha=0.2, levels=range(-1, 2))
+    pyplot.contour(xx, yy, Z, colors='k', linewidths=1)
     plot_points(X, y, size_of_points)
     pyplot.show()
-    
+
+
 def display_tree(dt):
-    from sklearn.externals.six import StringIO  
-    from IPython.display import Image  
+    from six import StringIO
+    from IPython.display import Image
     from sklearn.tree import export_graphviz
     import pydotplus
     dot_data = StringIO()
-    export_graphviz(dt, out_file=dot_data,  
+    export_graphviz(dt, out_file=dot_data,
                     filled=True, rounded=True,
                     special_characters=True)
-    graph = pydotplus.graph_from_dot_data(dot_data.getvalue())  
+    graph = pydotplus.graph_from_dot_data(dot_data.getvalue())
     return Image(graph.create_png())
 
+
 def plot_regressor(model, features, labels):
-    x = np.linspace(0,85,1000)
+    x = np.linspace(0, 85, 1000)
     pyplot.scatter(features, labels)
-    pyplot.plot(x, model.predict(x.reshape([-1,1])))
+    pyplot.plot(x, model.predict(x.reshape([-1, 1])))
     pyplot.xlabel("Age")
     pyplot.ylabel("Days per week")
     pyplot.show()
